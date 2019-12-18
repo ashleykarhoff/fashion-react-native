e00a1f880ea719e3f9123eb85a47fa7981e52b0a
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-12-18 14:53:55 -0500

    Installed react-native-deck-swiper and have working demo

---
+++ assets/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 4,
+    backgroundColor: "#F5FCFF"
+  },
+  card: {
+    flex: 0.75,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
+  }
+});
+
+export default styles;
--- components/Discovery.js
+++ components/Discovery.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { Button, View, Text } from "react-native";
 import { connect } from "react-redux";
+import Swiper from "react-native-deck-swiper";
 import { fetchItems } from "./../redux/actions";
-import Swipe from "./Swipe";
+// import Swipe from "./Swipe";
 import store from "../redux/store";
+import styles from "../assets/styles";
 
 class Discovery extends Component {
   state = {
@@ -18,12 +20,42 @@ class Discovery extends Component {
 
   render() {
     return (
-      <View style={{ flex: 4, justifyContent: "center", alignItems: "center" }}>
-        <Text>Discovery Container </Text>
-        {this.state.items.map(i => (
-          <Text key={i.id}>{i.name}</Text>
-        ))}
-        <Swipe />
+      //   <View style={{ flex: 4, justifyContent: "center", alignItems: "center" }}>
+      //     <Text>Discovery Container </Text>
+      //     {this.state.items.map(i => (
+      //       <Text key={i.id}>{i.name}</Text>
+      //     ))}
+      //     <Swipe />
+      //   </View>
+      <View style={styles.container}>
+        <Swiper
+          cards={["DO", "MORE", "OF", "WHAT", "MAKES", "YOU", "HAPPY"]}
+          renderCard={card => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.text}>{card}</Text>
+              </View>
+            );
+          }}
+          onSwiped={cardIndex => {
+            console.log(cardIndex);
+          }}
+          onSwipedAll={() => {
+            console.log("onSwipedAll");
+          }}
+          cardIndex={0}
+          backgroundColor={"#4FD0E9"}
+          stackSize={3}
+        >
+          {/* <Button
+            onPress={() => {
+              console.log("oulala");
+            }}
+            title="Press me"
+          >
+            You can press me
+          </Button> */}
+        </Swiper>
       </View>
     );
   }
