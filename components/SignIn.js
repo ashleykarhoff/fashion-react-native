d69b73c9bae4db8435aa9fd68f8f510972223a8d
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-12-29 13:36:34 -0500

    Fixed AsyncStorage import and created a very basic login page that still needs to set the userKey

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,13 +1,53 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { AsyncStorage } from "react-native";
+import { View, Text, Button } from "react-native";
+import { Formik } from "formik";
+import { TextInput } from "react-native-gesture-handler";
+import styles from "../assets/styles";
 
-class SignIn extends Component {
-  state = {};
+class SignIn extends React.Component {
+  handleSubmit = () => {
+    _retrieveData = async () => {
+      try {
+        const value = await AsyncStorage.getItem("@userKey");
+        if (value !== null) {
+          // We have data!!
+          console.log(value);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  };
+
   render() {
     return (
-      <View>
-        <Text>Signin</Text>
-      </View>
+      <Formik
+        style={styles.container}
+        initialValues={{ email: "", password: "" }}
+        onSubmit={values => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
+            <Text>Signin</Text>
+            <Text>Email</Text>
+            <TextInput
+              style={{ backgroundColor: "#ededed", height: 30 }}
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+            />
+            <Text>Password</Text>
+            <TextInput
+              style={{ backgroundColor: "#ededed", height: 30 }}
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+            <Button onPress={handleSubmit} title="Submit" />
+          </View>
+        )}
+      </Formik>
     );
   }
 }
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,10 +1,30 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { AsyncStorage } from "react-native";
 import Discovery from "../components/Discovery";
 import Board from "../components/Board";
 import Show from "../components/Show";
+import SignIn from "../components/SignIn";
+// import Signup from "../components/Signup";
+// import ResetPassword from "../components/ResetPassword";
+
+// state.userToken == null ? (
+//   <>
+//     <Stack.Screen name="SignIn" component={SignInScreen} />
+//     <Stack.Screen name="SignUp" component={SignUpScreen} />
+//     <Stack.Screen name="ResetPassword" component={ResetPassword} />
+//   </>
+// ) : (
+//   <>
+//     <Stack.Screen name="Home" component={HomeScreen} />
+//     <Stack.Screen name="Profile" component={ProfileScreen} />
+//   </>
+// );
 
 const screens = {
+  Signin: {
+    screen: SignIn
+  },
   Home: {
     screen: Discovery
   },
@@ -15,6 +35,39 @@ const screens = {
     screen: Show
   }
 };
+
+getMyValue = async () => {
+  try {
+    const token = await AsyncStorage.getItem("@userEmail");
+    console.log(token);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// this.getMyValue() === null
+//   ? (screens = {
+//       SignIn: {
+//         screen: SignIn
+//       },
+//       SignUp: {
+//         screen: Signup
+//       },
+//       ResetPassword: {
+//         screen: ResetPassword
+//       }
+//     })
+//   : (screens = {
+//       Home: {
+//         screen: Discovery
+//       },
+//       Board: {
+//         screen: Board
+//       },
+//       Show: {
+//         screen: Show
+//       }
+//     });
 
 const HomeStack = createStackNavigator(screens);
 
