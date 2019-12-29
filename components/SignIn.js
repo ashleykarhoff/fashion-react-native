1d3f3fe898e184b84a8bffb58e9a7c8b42152ec9
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-12-29 13:47:09 -0500

    Added secureTextEntry to password field

--- components/SignIn.js
+++ components/SignIn.js
@@ -6,26 +6,32 @@ import { TextInput } from "react-native-gesture-handler";
 import styles from "../assets/styles";
 
 class SignIn extends React.Component {
-  handleSubmit = () => {
-    _retrieveData = async () => {
-      try {
-        const value = await AsyncStorage.getItem("@userKey");
-        if (value !== null) {
-          // We have data!!
-          console.log(value);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  };
+  //   _storeData = async key => {
+  //     try {
+  //       const value = await AsyncStorage.setItem("@MySuperStore:key", key.email);
+  //       console.log(value);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //     _retrieveData = async () => {
+  //       try {
+  //         const key = await AsyncStorage.getItem("@userKey");
+  //         if (key !== null) {
+  //           this._storeData(key);
+  //         }
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     };
 
   render() {
     return (
       <Formik
         style={styles.container}
         initialValues={{ email: "", password: "" }}
-        onSubmit={values => console.log(values)}
+        onSubmit={values => values}
       >
         {({ handleChange, handleBlur, handleSubmit, values }) => (
           <View>
@@ -43,6 +49,7 @@ class SignIn extends React.Component {
               onChangeText={handleChange("password")}
               onBlur={handleBlur("password")}
               value={values.password}
+              secureTextEntry
             />
             <Button onPress={handleSubmit} title="Submit" />
           </View>
